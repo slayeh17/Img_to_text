1c46515932f336144e7aa2ddf1034e0c4f5f8ad1
debak roy <debakroy7@gmail.com>
2023-08-12 22:00:52 +0530

    update api

--- imgTxt/urls.py
+++ imgTxt/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('extract/', views.extract_text_from_image, name='extract_text'),
-    path('api/extract/<str:image_url>/', views.extract_text_api, name='extract_text_api'),
+    path('extract/', views.extract_text_from_image, name='extract_text_from_image'),
+    path('extract/api/', views.extract_text_api, name='extract_text_api'),
 ]
--- imgTxt/views.py
+++ imgTxt/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 import pytesseract
 from PIL import Image
 import requests
+from urllib.parse import unquote
 from io import BytesIO
 
 def extract_text_from_image(request):
@@ -25,20 +26,22 @@ def extract_text_from_image(request):
             return JsonResponse({'error': 'Failed to download image'})
     return render(request, 'imgTxt/layout.html')
 
-def extract_text_api(request, image_url):
-    response = requests.get(image_url)
-    if response.status_code == 200:
-        image = Image.open(BytesIO(response.content))
-        extracted_text = pytesseract.image_to_string(image)
-            
-        text_lines = extracted_text.split('\n')
-        text_lines_cpy = []
-        for text in text_lines:
-            if len(text) > 1:
-                text_lines_cpy.append(text.capitalize())
-        text_dict = {f'{i + 1}': line.strip() for i, line in enumerate(text_lines_cpy)}
-        
-        return JsonResponse(text_dict)
-    else:
-        return JsonResponse({'error': 'Failed to download image'})
+def extract_text_api(request):
+    image_url = request.GET.get('url')
 
+    if image_url:
+        decoded_image_url = unquote(image_url)
+        response = requests.get(decoded_image_url)
+        
+        if response.status_code == 200:
+            image = Image.open(BytesIO(response.content))
+            extracted_text = pytesseract.image_to_string(image)
+
+            text_lines = extracted_text.split('\n')
+            text_dict = {f'line_{i + 1}': line.strip() for i, line in enumerate(text_lines) if line.strip()}
+
+            return JsonResponse(text_dict)
+        else:
+            return JsonResponse({'error': 'Failed to download image'})
+    else:
+        return JsonResponse({'error': 'Missing or invalid URL parameter'})
